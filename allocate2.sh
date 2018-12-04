4fde11a51057da1f6606fffcae300fb820bdcae8
Codefresh <ziv@codefresh.io>
2018-12-04 12:32:36 +0200

    up

--- allocate2.sh
+++ allocate2.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-for index in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15; do
-    value=$(($index * 1024))
-    eval array$index=\"array[$index]: $value\"
+echo "begin allocating memory..."
+for index in $(seq 1000); do
+    value=$(seq -w -s '' $index $(($index + 100000)))
+    eval array$index=$value
 done
-
-for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15; do
-    eval echo \$array$i
-done
+echo "...end allocating memory"
