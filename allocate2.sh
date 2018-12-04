0186489433dc744568f15d8887739ce0de7314d5
Codefresh <ziv@codefresh.io>
2018-12-04 14:03:54 +0200

    up

--- allocate2.sh
+++ allocate2.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 echo "begin allocating memory..."
-free -m
-for index in $(seq 1000000); do
-    value=$(seq -w -s '' $index $(($index + 10000000)))
+for index in $(seq 1000); do
+    eval array$index=1000000000000000000000
     free -m
 done
 echo "...end allocating memory"
