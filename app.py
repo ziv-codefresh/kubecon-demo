319e786885fb71a87521c2ed27a7d4c844c5b696
ziv-codefresh <32479607+ziv-codefresh@users.noreply.github.com>
2018-12-09 11:53:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 print ('start allocation')
 some_str = ' '
-for x in range(0, 5):
-    some_str =  some_str + ' ' * 1048576 * 500
-    print ('Allocate 500Mi')
+for x in range(0, 500):
+    some_str =  some_str + (' ' * 1048576 * 5)
+    print ('Allocate 5Mi')
 print ('finish allocation')
