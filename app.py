ad233fb83c78cc86651a9fcf3cd6f2d204c68ea3
ziv-codefresh <32479607+ziv-codefresh@users.noreply.github.com>
2018-12-09 08:44:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
 print ('start allocation')
-some_str =  ' ' * 1048576 * 3500
+for x in range(0, 5):
+    some_str =  some_str + ' ' * 1048576 * 500
+    print "We're on time %d" % (x)
 print ('finish allocation')
