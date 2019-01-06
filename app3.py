07c63496fca994a24db9b0dd92953bf909785ce3
ziv-codefresh <32479607+ziv-codefresh@users.noreply.github.com>
2019-01-06 14:08:04 +0200

    Create app3.py

---
+++ app3.py
@@ -0,0 +1,12 @@
+from multiprocessing import Pool
+
+def f(x):
+    # Put any cpu (only) consuming operation here. I have given 1 below -
+    while True:
+        x * x
+
+# decide how many cpus you need to load with.
+no_of_cpu_to_be_consumed = 1
+
+p = Pool(processes=no_of_cpu_to_be_consumed)
+p.map(f, range(no_of_cpu_to_be_consumed))
