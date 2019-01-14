7274a6e48438288f9908d343d21dcb35c5d73610
ziv-codefresh <32479607+ziv-codefresh@users.noreply.github.com>
2019-01-14 15:31:09 +0200

    Update app3.py

--- app3.py
+++ app3.py
@@ -1,12 +1,10 @@
-from multiprocessing import Pool
-
-def f(x):
-    # Put any cpu (only) consuming operation here. I have given 1 below -
-    while True:
-        x * x
-
-# decide how many cpus you need to load with.
-no_of_cpu_to_be_consumed = 1
-
-p = Pool(processes=no_of_cpu_to_be_consumed)
-p.map(f, range(no_of_cpu_to_be_consumed))
+import time
+print ('start allocation')
+some_str = ' '
+size = 0
+for x in range(0, 100):
+    some_str =  some_str + (' ' * 1 * 50)
+    print ('Allocated ' +str(size)+ 'Mi')
+    size = size+50
+    time.sleep(1)
+print ('finish allocation')
