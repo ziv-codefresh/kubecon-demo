bae79fc78ccd08c66451e59f86dc4f54d4404c3f
ziv-codefresh <32479607+ziv-codefresh@users.noreply.github.com>
2019-01-14 15:39:56 +0200

    Create app4.py

---
+++ app4.py
@@ -0,0 +1,10 @@
+import time
+print ('start allocation')
+some_str = ' '
+size = 0
+for x in range(0, 1000):
+    some_str =  some_str + (' ' * 1 * 50)
+    print ('Allocated ' +str(size)+ 'Mi')
+    size = size+50
+    time.sleep(1)
+print ('finish allocation')
