0fe89dba61a0bb135ddf480533bb90ddabef8c76
ziv-codefresh <32479607+ziv-codefresh@users.noreply.github.com>
2019-02-21 09:46:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import time
 print ('start allocation')
 some_str = ' '
 size = 0
-for x in range(0, 75):
-    some_str =  some_str + (' ' * 1048576 * 50)
+for x in range(0, 71000):
+    #some_str =  some_str + (' ' * 1048576 * 50)
     print ('Allocated ' +str(size)+ 'Mi')
-    size = size+50
-    time.sleep(0.5)
+    #size = size+50
+    time.sleep(3)
 some_str = ' '
 time.sleep(5)
 
